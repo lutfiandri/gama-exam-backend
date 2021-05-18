cc5b36af9ace8e8cc325f35f5452987e5cd8452f
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2021-05-18 20:43:03 +0700

    Completing Basic CRUD Controller

--- GamaExamBackend/Startup.cs
+++ GamaExamBackend/Startup.cs
@@ -28,14 +28,10 @@ namespace GamaExamBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+			services.AddControllers();
             services.AddDbContext<DBExamContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GamaExamBackend", Version = "v1" });
-            });
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,11 +40,15 @@ namespace GamaExamBackend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GamaExamBackend v1"));
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -56,7 +56,9 @@ namespace GamaExamBackend
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
